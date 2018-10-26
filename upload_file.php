8b3381d5bda2b0afa0cc3d47dcbe8660bf4d417b
Joshua <josh.rdgrs@yahoo.com>
2018-10-26 07:09:01 -0500

    inserted generic file uploading script from Ullman book to modify.

---
+++ upload_file.php
@@ -0,0 +1,49 @@
+<?php
+  include('templates/selectscreen-header.html');
+?>
+    <main class="content">
+      <div class="post-form">
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+          if (move_uploaded_file ($_FILES['the_file']['tmp_name'], "../uploads/{$_FILES['the_file']['name']}")) {
+              print '<p>Your file has been uploaded.</p>';
+            } else {
+              print '<p style="color: red;">Your file could not be uploaded because: ';
+
+              switch ($_FILES['the_file']['error']) {
+                case 1:
+                  print 'The file exceeds the upload_max_filesize setting in php.ini';
+                  break;
+                case 2:
+                  print 'The exceeds the MAX_FILE_SIZE setting in the HTML form';
+                  break;
+                case 3:
+                  print 'The file was only partially uploaded';
+                  break;
+                case 4:
+                  print 'No file was uploaded';
+                  break;
+                case 6:
+                  print 'The temporary folder does not exist.';
+                  break;
+                default:
+                  print 'Something unexpected happened.';
+                  break;
+              }
+              print '.</p>';
+
+            }
+
+        }
+        ?>
+      <form action="upload_file.php" enctype="multipart/form-data" method="post">
+        <p>Upload a file using this form:</p>
+        <input type="hidden" name="MAX_FILE_SIZE" value="300000"/>
+        <p><input type="file" name="the_file"/></p>
+        <p><input type="submit" name="submit" value="Upload This File"/></p>
+      </form>
+      </div>
+    </main>
+<?php
+  include('templates/footer.html');
+?>
